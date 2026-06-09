0655c515bfcc132e3b09b1e8f235f92510eb0c21
LineTechSA <abuabedgames1@gmail.com>
2026-06-09 11:35:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
+
 from services.resident_evil.pre_ar import get_pre_ar_news
+from services.rockstar.rockstar_news import get_rockstar_news
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 
-    news = get_pre_ar_news()
+    resident_news = get_pre_ar_news()
+    rockstar_news = get_rockstar_news()
 
     return render_template(
         "index.html",
-        resident_evil_news=news
+        resident_evil_news=resident_news,
+        rockstar_news=rockstar_news
     )
 
 if __name__ == "__main__":
