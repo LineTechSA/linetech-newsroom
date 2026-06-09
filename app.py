5364b10298b05090c266e6edf4e91f2729a42d0a
LineTechSA <abuabedgames1@gmail.com>
2026-06-09 11:20:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from services.resident_evil.pre_ar import get_pre_ar_news
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    news = get_pre_ar_news()
+
+    return render_template(
+        "index.html",
+        resident_evil_news=news
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
