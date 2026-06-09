770fedd081011829fcf2ee84c19876bd720403b5
LineTechSA <abuabedgames1@gmail.com>
2026-06-09 11:59:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 from flask import Flask, render_template
 
 from services.resident_evil.pre_ar import get_pre_ar_news
-from services.rockstar.rockstar_news import get_rockstar_news
+from ai.package_generator import generate_package
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 
-    resident_news = get_pre_ar_news()
-    rockstar_news = get_rockstar_news()
+    news = get_pre_ar_news()
 
     return render_template(
         "index.html",
-        resident_evil_news=resident_news,
-        rockstar_news=rockstar_news
+        resident_evil_news=news
     )
+
+@app.route("/generate")
+def generate():
+
+    result = generate_package(
+        "كشف الستار عن خفايا عرض Resident Evil Requiem!"
+    )
+
+    return f"""
+    <h1>📦 Gemini Package</h1>
+
+    <pre style="
+    white-space:pre-wrap;
+    font-size:18px;
+    padding:20px;
+    ">
+    {result}
+    </pre>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
